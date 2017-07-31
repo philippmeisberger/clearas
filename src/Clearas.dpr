705fdc03310ac5c220862c0e09ff6aec73048131
Philipp Meisberger <team@pm-codeworks.de>
2017-07-31 15:29:06 +0200

    Disabled mutex for debug builds; Enabled leak-checking for debug builds

--- src/Clearas.dpr
+++ src/Clearas.dpr
@@ -5,13 +5,18 @@ program Clearas;
 
 uses
   Forms,
+{$IFNDEF DEBUG}
   PMCW.Application.Mutex,
+{$ENDIF}
   ClearasMain in 'ClearasMain.pas' {Main},
   ClearasAPI in 'ClearasAPI.pas';
 
 {$R *.res}
 
 begin
+{$IFDEF DEBUG}
+  ReportMemoryLeaksOnShutdown := True;
+{$ENDIF}
   Application.Initialize;
   Application.Title := 'Clearas';
   Application.MainFormOnTaskbar := True;
